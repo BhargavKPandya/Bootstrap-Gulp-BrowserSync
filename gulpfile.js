0b9e041da87fbecd813ac901cff1e3cee88b06b3
BhargavKPandya <BhargavKPandya.22@gmail.com>
2019-04-19 22:18:56 -0400

    Delete Synchronization during watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,118 @@
+/**
+ * File name: gulpfile.js
+ * Author: Bhargav Pandya
+ * Version: 1.2
+ * Description: Delete files from build folder during watch task, Delete build Directory at initial Gulp task 
+ */
+
 var gulp = require('gulp');
 //var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var uglify = require('gulp-uglify');
 var cache = require('gulp-cache');
+var del = require('del');
+var path = require('path');
+const dir = {
+  src: {
+    path: "./src/",
+    images: "./src/images/",
+    css: "./src/css/",
+    js: "./src/js/",
+  },
+  dist: {
+    path: "./dist/",
+    images: "./dist/images/",
+    css: "./dist/css/",
+    js: "./dist/js/",
+  }
+};
 
+const extension = {
+  template: "*.html",
+  style: "*.css",
+  image: "*",
+  script: "*.js"
+};
 
-gulp.task('browser-sync', function(){
+gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
       baseDir: './dist'
     },
-})
+  })
 });
 
-function copyhtml () {
-   return gulp.src('./src/*.html')
-        .pipe(gulp.dest('./dist/'))
-        .pipe(browserSync.reload({stream: true}));
- }
-
- function copyImage () {
-  return gulp.src('./src/images/*', {since: gulp.lastRun(copyImage)})
-       .pipe(gulp.dest('./dist/images/'))
-       .pipe(browserSync.reload({stream: true}));
+function reload() {
+  browserSync.reload();
 }
 
- function copyCSS () {
-  return gulp.src('./src/css/*.css', {since: gulp.lastRun(copyCSS)})
-       .pipe(gulp.dest('./dist/css/'))
-       .pipe(browserSync.reload({stream: true}));
+function cleanDist(){
+  return del(`${dir.dist.path}`);
 }
 
-function copyJS () {
-  return gulp.src('./src/js/*.js', {since: gulp.lastRun(copyJS)})
-       .pipe(gulp.dest('./dist/js/'))
-       .pipe(browserSync.reload({stream: true}));
+function copyhtml() {
+  return gulp.src(`${dir.src.path + extension.template}`)
+    .pipe(gulp.dest(`${dir.dist.path}`))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-function watchhtml () {
-  return gulp.watch("./src/*.html", gulp.parallel(copyhtml));
+function copyImage() {
+  return gulp.src(`${dir.src.images + extension.image}`, { since: gulp.lastRun(copyImage) })
+    .pipe(gulp.dest(`${dir.dist.images}`))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-function watchCSS () {
- return gulp.watch("./src/css/*.css", gulp.series(copyCSS, function(done){
-   browserSync.reload();
-   done();
- }));
+function copyCSS() {
+  return gulp.src(`${dir.src.css + extension.style}`, { since: gulp.lastRun(copyCSS) })
+    .pipe(gulp.dest(`${dir.dist.css}`))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-function watchJS () {
- return gulp.watch("./src/js/*.js", gulp.parallel(copyJS));
+function copyJS() {
+  return gulp.src(`${dir.src.js + extension.script}`, { since: gulp.lastRun(copyJS) })
+    .pipe(gulp.dest(`${dir.dist.js}`))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-function watchImage () {
-  return gulp.watch("./src/images/*", gulp.parallel(copyImage));
+function watchhtml() {
+  return gulp.watch(`${dir.src.path + extension.template}`, gulp.parallel(copyhtml));
 }
+
+function watchCSS() {
+  return gulp.watch(`${dir.src.css + extension.style}`, gulp.series(copyCSS, function (done) {
+    browserSync.reload();
+    done();
+  }))
+    .on('unlink', function (filepath) {
+      var filePathFromSrc = path.relative(path.resolve(`${dir.src.css}`), filepath);
+      var destFilePath = path.resolve(`${dir.dist.css}`, filePathFromSrc);
+      del.sync(destFilePath);
+      reload();
+    });
+}
+
+function watchJS() {
+  return gulp.watch(`${dir.src.js + extension.script}`, gulp.parallel(copyJS))
+    .on('unlink', function (filepath) {
+      var filePathFromSrc = path.relative(path.resolve(`${dir.src.js}`), filepath);
+      var destFilePath = path.resolve(`${dir.dist.js}`, filePathFromSrc);
+      del.sync(destFilePath);
+      reload();
+    });
+}
+
+function watchImage() {
+  return gulp.watch(`${dir.src.images + extension.image}`, gulp.parallel(copyImage))
+    .on('unlink', function (filepath) {
+      var filePathFromSrc = path.relative(path.resolve(`${dir.src.images}`), filepath);
+      var destFilePath = path.resolve(`${dir.dist.images}`, filePathFromSrc);
+      del.sync(destFilePath);
+      reload();
+    });
+}
+
+
 
 gulp.task('copy', gulp.parallel(copyCSS, copyJS, copyhtml, copyImage));
-gulp.task('watch', gulp.parallel( watchCSS, watchhtml, watchJS, watchImage))
-gulp.task('default',gulp.parallel('browser-sync','copy','watch'));
+gulp.task('watch', gulp.parallel(watchCSS, watchhtml, watchJS, watchImage));
+gulp.task('default',gulp.series(cleanDist, gulp.parallel('browser-sync', 'copy', 'watch')));
